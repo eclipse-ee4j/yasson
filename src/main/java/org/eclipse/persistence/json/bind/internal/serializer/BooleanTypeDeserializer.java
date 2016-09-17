bfa8202ef5d061091cdbf370e1835199050902ab
Ehsan Zaery Moghaddam <zaerymoghaddam@gmail.com>
2016-09-17 12:39:18 +0200

    Fix issue #2 - Failed to deserialize boolean literal (true/false)

--- src/main/java/org/eclipse/persistence/json/bind/internal/serializer/BooleanTypeDeserializer.java
+++ src/main/java/org/eclipse/persistence/json/bind/internal/serializer/BooleanTypeDeserializer.java
@@ -13,9 +13,15 @@
 
 package org.eclipse.persistence.json.bind.internal.serializer;
 
+import org.eclipse.persistence.json.bind.internal.JsonbParser;
 import org.eclipse.persistence.json.bind.internal.Unmarshaller;
+import org.eclipse.persistence.json.bind.internal.properties.MessageKeys;
+import org.eclipse.persistence.json.bind.internal.properties.Messages;
 import org.eclipse.persistence.json.bind.model.JsonBindingModel;
 
+import javax.json.bind.JsonbException;
+import javax.json.bind.serializer.DeserializationContext;
+import javax.json.stream.JsonParser;
 import java.lang.reflect.Type;
 
 /**
@@ -25,6 +31,21 @@ public class BooleanTypeDeserializer extends AbstractValueTypeDeserializer<Boole
 
     public BooleanTypeDeserializer(JsonBindingModel model) {
         super(Boolean.class, model);
+    }
+
+    @Override
+    public Boolean deserialize(JsonParser parser, DeserializationContext ctx, Type rtType) {
+        final JsonParser.Event event = ((JsonbParser) parser).moveToValue();
+        switch (event) {
+            case VALUE_TRUE:
+                return Boolean.TRUE;
+            case VALUE_FALSE:
+                return Boolean.FALSE;
+            case VALUE_STRING:
+                return Boolean.parseBoolean(parser.getString());
+            default:
+                throw new JsonbException(Messages.getMessage(MessageKeys.INTERNAL_ERROR, "Unknown JSON value: " + event));
+        }
     }
 
     @Override
