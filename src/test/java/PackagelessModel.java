0f9bc81b6c7ebde5a651a35a427096f094dcbb5f
Ehsan Zaery Moghaddam <zaerymoghaddam@gmail.com>
2016-12-02 16:09:25 +0100

    Fix serialization/deserialization of a class that has no package
    Signed-off-by: Ehsan Zaery Moghaddam <zaerymoghaddam@gmail.com>

--- src/test/java/PackagelessModel.java
+++ src/test/java/PackagelessModel.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Ehsan Zaery Moghaddam (zaerymoghaddam@gmail.com) - initial implementation
+ ******************************************************************************/
+
 /**
  * This class is designed outside any packages intentionally to test serialization/deserialization scenarios for such
  * cases
