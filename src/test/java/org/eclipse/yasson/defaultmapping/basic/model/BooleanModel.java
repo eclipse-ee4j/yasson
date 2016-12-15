b8d7ac96864df040a8854e03ee75404de7ae96f4
Ehsan Zaery Moghaddam <zaerymoghaddam@gmail.com>
2016-12-15 19:44:57 +0100

    Fix boolean value serialization issue - #2
    Signed-off-by: Ehsan Zaery Moghaddam <zaerymoghaddam@gmail.com>

--- src/test/java/org/eclipse/yasson/defaultmapping/basic/model/BooleanModel.java
+++ src/test/java/org/eclipse/yasson/defaultmapping/basic/model/BooleanModel.java
@@ -1,10 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Ehsan Zaery Moghaddam (zaerymoghaddam@gmail.com)
+ ******************************************************************************/
+
 package org.eclipse.yasson.defaultmapping.basic.model;
 
 /**
  * Encapsulates different types of boolean values as a field so that the boolean value's serialization and
  * deserialization could be tested
  *
- * Created by Ehsan Zaery Moghaddam (zaerymoghaddam@gmail.com) on 9/15/16.
+ * @author Ehsan Zaery Moghaddam (zaerymoghaddam@gmail.com)
  */
 public class BooleanModel {
     public Boolean field1;
