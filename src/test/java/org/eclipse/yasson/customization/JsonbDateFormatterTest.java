eed57470a8b2c8a16eda8208c07e6f04ac45303c
Ehsan Zaery Moghaddam <zaerymoghaddam@gmail.com>
2017-03-07 11:54:19 +0100

    yasson-9 Fix time zone issue on integration tests (#16)
    Signed-off-by: Ehsan Zaery Moghaddam <zaerymoghaddam@gmail.com>

--- src/test/java/org/eclipse/yasson/customization/JsonbDateFormatterTest.java
+++ src/test/java/org/eclipse/yasson/customization/JsonbDateFormatterTest.java
@@ -20,7 +20,6 @@ import org.junit.Test;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import java.util.Calendar;
-import java.util.TimeZone;
 
 import static org.junit.Assert.assertEquals;
 
@@ -38,7 +37,6 @@ public class JsonbDateFormatterTest {
         final Calendar timeCalendar = new Calendar.Builder()
                 .setDate(2017, Calendar.MARCH, 3)
                 .setTimeOfDay(11, 11, 10)
-                .setTimeZone(TimeZone.getTimeZone("Europe/Copenhagen"))
                 .build();
 
         DateFormatPojo dateFormatPojo = new DateFormatPojo();
@@ -61,7 +59,6 @@ public class JsonbDateFormatterTest {
         final Calendar timeCalendar = new Calendar.Builder()
                 .setDate(2017, Calendar.MARCH, 3)
                 .setTimeOfDay(11, 11, 10)
-                .setTimeZone(TimeZone.getTimeZone("Europe/Copenhagen"))
                 .build();
 
         DateFormatPojo result = jsonb.fromJson("{\"formattedDateField\":\"11:11:10 ^^ 03-03-2017\",\"getterAndSetterAndFieldFormattedDateField\":\"11:11:10 $$ 03-03-2017\",\"getterAndSetterFormattedDateField\":\"11:11:10 <> 03-03-2017\",\"getterAndFieldFormattedDateField\":\"11:11:10 ^^ 03-03-2017\",\"getterFormattedDateField\":\"2017-03-03T11:11:10\",\"plainDateField\":\"2017-03-03T11:11:10\",\"setterAndFieldFormattedDateField\":\"11:11:10 <> 03-03-2017\",\"setterFormattedDateField\":\"11:11:10 ^^ 03-03-2017\"}", DateFormatPojo.class);
@@ -81,7 +78,6 @@ public class JsonbDateFormatterTest {
         final Calendar timeCalendar = new Calendar.Builder()
                 .setDate(2017, Calendar.MARCH, 3)
                 .setTimeOfDay(11, 11, 10)
-                .setTimeZone(TimeZone.getTimeZone("Europe/Copenhagen"))
                 .build();
 
         DateFormatPojoWithClassLevelFormatter dateFormatPojo = new DateFormatPojoWithClassLevelFormatter();
@@ -104,7 +100,6 @@ public class JsonbDateFormatterTest {
         final Calendar timeCalendar = new Calendar.Builder()
                 .setDate(2017, Calendar.MARCH, 3)
                 .setTimeOfDay(11, 11, 10)
-                .setTimeZone(TimeZone.getTimeZone("Europe/Copenhagen"))
                 .build();
 
         DateFormatPojoWithClassLevelFormatter result = jsonb.fromJson("{\"formattedDateField\":\"11:11:10 ^^ 03-03-2017\",\"getterAndSetterAndFieldFormattedDateField\":\"11:11:10 $$ 03-03-2017\",\"getterAndSetterFormattedDateField\":\"11:11:10 <> 03-03-2017\",\"getterAndFieldFormattedDateField\":\"11:11:10 ^^ 03-03-2017\",\"getterFormattedDateField\":\"11:11:10 ^ 03-03-2017\",\"plainDateField\":\"11:11:10 ^ 03-03-2017\",\"setterAndFieldFormattedDateField\":\"11:11:10 <> 03-03-2017\",\"setterFormattedDateField\":\"11:11:10 ^^ 03-03-2017\"}", DateFormatPojoWithClassLevelFormatter.class);
