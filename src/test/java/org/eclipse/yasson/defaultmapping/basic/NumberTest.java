f2ced7e4c28afd1d30028707f317192a59f5fc38
David Kral <david.k.kral@oracle.com>
2017-03-31 13:15:30 +0200

    Big number format fix

--- src/test/java/org/eclipse/yasson/defaultmapping/basic/NumberTest.java
+++ src/test/java/org/eclipse/yasson/defaultmapping/basic/NumberTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 /**
  * @author Roman Grigoriadi
@@ -28,18 +29,54 @@ public class NumberTest {
     @Test
     public void testBigDecimalMarshalling() {
         String jsonString = jsonb.toJson(new BigDecimal("0.10000000000000001"));
-        Assert.assertEquals("0.10000000000000001", jsonString);
+        Assert.assertEquals("\"0.10000000000000001\"", jsonString);
+
+        jsonString = jsonb.toJson(new BigDecimal("0.1000000000000001"));
+        Assert.assertEquals("0.1000000000000001", jsonString);
 
         BigDecimal result = jsonb.fromJson("0.10000000000000001", BigDecimal.class);
         Assert.assertEquals(new BigDecimal("0.10000000000000001"), result);
+
+        result = jsonb.fromJson("\"0.100000000000000001\"", BigDecimal.class);
+        Assert.assertEquals(new BigDecimal("0.100000000000000001"), result);
+    }
+
+    @Test
+    public void testBigDecimalIEEE748() {
+        String jsonString = jsonb.toJson(new BigDecimal("9007199254740991"));
+        Assert.assertEquals("9007199254740991", jsonString);
+
+        jsonString = jsonb.toJson(new BigDecimal("9007199254740992"));
+        Assert.assertEquals("\"9007199254740992\"", jsonString);
+
+        jsonString = jsonb.toJson(new BigDecimal("9007199254740991.1"));
+        Assert.assertEquals("\"9007199254740991.1\"", jsonString);
+
+        jsonString = jsonb.toJson(new BigDecimal(new BigInteger("1"), -400));
+        Assert.assertEquals("\"" + new BigDecimal(new BigInteger("1"), -400) + "\"", jsonString);
+    }
+
+    @Test
+    public void testBigIntegerIEEE748() {
+        String jsonString = jsonb.toJson(new BigInteger("9007199254740991"));
+        Assert.assertEquals("9007199254740991", jsonString);
+
+        jsonString = jsonb.toJson(new BigInteger("9007199254740992"));
+        Assert.assertEquals("\"9007199254740992\"", jsonString);
     }
 
     @Test
     public void testBigDecimalWrappedMarshalling() {
-        String jsonString = jsonb.toJson(new ScalarValueWrapper<>(new BigDecimal("0.10000000000000001")));
-        Assert.assertEquals("{\"value\":0.10000000000000001}", jsonString);
+        String jsonString = jsonb.toJson(new ScalarValueWrapper<>(new BigDecimal("0.1000000000000001")));
+        Assert.assertEquals("{\"value\":0.1000000000000001}", jsonString);
 
-        ScalarValueWrapper<BigDecimal> result = jsonb.fromJson("{\"value\":0.10000000000000001}", new TestTypeToken<ScalarValueWrapper<BigDecimal>>(){}.getType());
+        jsonString = jsonb.toJson(new ScalarValueWrapper<>(new BigDecimal("0.10000000000000001")));
+        Assert.assertEquals("{\"value\":\"0.10000000000000001\"}", jsonString);
+
+        ScalarValueWrapper<BigDecimal> result = jsonb.fromJson("{\"value\":0.1000000000000001}", new TestTypeToken<ScalarValueWrapper<BigDecimal>>(){}.getType());
+        Assert.assertEquals(new BigDecimal("0.1000000000000001"), result.getValue());
+
+        result = jsonb.fromJson("{\"value\":\"0.10000000000000001\"}", new TestTypeToken<ScalarValueWrapper<BigDecimal>>(){}.getType());
         Assert.assertEquals(new BigDecimal("0.10000000000000001"), result.getValue());
     }
 
