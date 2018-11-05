61c416f16e3a7f5725315a91bc5e9839717ea214
KriechelD <dennis.kriechel+github@gmail.com>
2018-11-05 19:37:10 +0100

    Possible Fix for yasson-172
    Signed-off-by: Dennis Kriechel <dennis.kriechel+github@gmail.com>

--- src/main/java/org/eclipse/yasson/internal/serializer/DateTypeDeserializer.java
+++ src/main/java/org/eclipse/yasson/internal/serializer/DateTypeDeserializer.java
@@ -13,13 +13,15 @@
 
 package org.eclipse.yasson.internal.serializer;
 
-import org.eclipse.yasson.internal.model.customization.Customization;
-
 import java.time.Instant;
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.TemporalAccessor;
 import java.util.Date;
 import java.util.Locale;
+
+import org.eclipse.yasson.internal.model.customization.Customization;
 
 /**
  * Deserializer for {@link Date} type.
@@ -28,32 +30,61 @@ import java.util.Locale;
  */
 public class DateTypeDeserializer extends AbstractDateTimeDeserializer<Date> {
 
-    private static final DateTimeFormatter DEFAULT_DATE_TIME_FORMATTER = DateTimeFormatter.ISO_DATE_TIME.withZone(UTC);
+	private static final DateTimeFormatter DEFAULT_DATE_TIME_FORMATTER = DateTimeFormatter.ISO_DATE_TIME;
 
-    /**
-     * Creates an instance.
-     *
-     * @param customization Model customization.
-     */
-    public DateTypeDeserializer(Customization customization) {
-        super(Date.class, customization);
-    }
+	/**
+	 * Creates an instance.
+	 *
+	 * @param customization Model customization.
+	 */
+	public DateTypeDeserializer(Customization customization) {
+		super(Date.class, customization);
+	}
 
+	@Override
+	protected Date fromInstant(Instant instant) {
+		return new Date(instant.toEpochMilli());
+	}
 
-    @Override
-    protected Date fromInstant(Instant instant) {
-        return new Date(instant.toEpochMilli());
-    }
-
-    @Override
+	@Override
     protected Date parseDefault(String jsonValue, Locale locale) {
-        final TemporalAccessor parsed = DEFAULT_DATE_TIME_FORMATTER.withLocale(locale).parse(jsonValue);
+		TemporalAccessor parsed;
+		
+		if (hasOffset(jsonValue)) {
+			parsed = DEFAULT_DATE_TIME_FORMATTER.withLocale(locale).parse(jsonValue);
+		} else {
+			parsed = DEFAULT_DATE_TIME_FORMATTER.withZone(UTC).withLocale(locale).parse(jsonValue);
+		}
+    	
         return new Date(Instant.from(parsed).toEpochMilli());
     }
 
-    @Override
-    protected Date parseWithFormatter(String jsonValue, DateTimeFormatter formatter) {
-        final TemporalAccessor parsed = getZonedFormatter(formatter).parse(jsonValue);
-        return new Date(Instant.from(parsed).toEpochMilli());
-    }
+	@Override
+	protected Date parseWithFormatter(String jsonValue, DateTimeFormatter formatter) {
+		TemporalAccessor parsed;
+		
+		if (hasOffset(jsonValue)) {
+			parsed = formatter.parse(jsonValue);
+		} else {
+			parsed = formatter.withZone(UTC).parse(jsonValue);
+		}
+		
+		return new Date(Instant.from(parsed).toEpochMilli());
+	}
+
+	/**
+	 * Checks if a json date value contains an offset in terms of
+	 * java.time.OffsetDateTime
+	 * 
+	 * @param jsonValue Value from json
+	 * @return true if the value could be interpreted as an date with an offset
+	 */
+	private boolean hasOffset(String jsonValue) {
+		try {
+			OffsetDateTime.parse(jsonValue);
+			return true;
+		} catch (DateTimeParseException e1) {
+			return false;
+		}
+	}
 }
