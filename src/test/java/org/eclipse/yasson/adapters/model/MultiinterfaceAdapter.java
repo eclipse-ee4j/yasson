4a9f57274252d51641a9c1a1c2c21f71ffeefd54
Tomas Kraus <Tomas.Kraus@oracle.com>
2019-01-28 16:57:51 +0100

    Initial Yasson website at Eclipse.
    Signed-off-by: Tomas Kraus <Tomas.Kraus@oracle.com>

--- src/test/java/org/eclipse/yasson/adapters/model/MultiinterfaceAdapter.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.yasson.adapters.model;
-
-import javax.json.bind.adapter.JsonbAdapter;
-import java.io.Serializable;
-
-interface MultiinterfaceAdapter<X, T> extends Serializable, JsonbAdapter<X, T> {
-
-
-}
--- src/test/java/org/eclipse/yasson/defaultmapping/generics/model/TypeContainer.java
+++
@@ -1,6 +0,0 @@
-package org.eclipse.yasson.defaultmapping.generics.model;
-
-public interface TypeContainer<T> {
-    T getInstance();
-    void setInstance(T instance);
-}
