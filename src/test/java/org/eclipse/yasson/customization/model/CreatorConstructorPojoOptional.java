949fbd5f63890d8373fbd830a28b5a5efb1e89b3
Minto van der Sluis <minto@xup.nl>
2019-06-25 00:11:07 +0200

    Fixed #276: Added special handling for Optional<> creator arguments
    Signed-off-by: Minto van der Sluis <minto@xup.nl>

--- src/test/java/org/eclipse/yasson/customization/model/CreatorConstructorPojoOptional.java
+++ src/test/java/org/eclipse/yasson/customization/model/CreatorConstructorPojoOptional.java
@@ -1,19 +1,4 @@
-/*******************************************************************************
- * Copyright (c) 2016 Oracle and/or its affiliates. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
- * which accompanies this distribution.
- * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at
- * http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * Contributors:
- * Roman Grigoriadi
- ******************************************************************************/
-
 package org.eclipse.yasson.customization.model;
-
-import org.eclipse.yasson.customization.model.CreatorFactoryMethodPojo;
 
 import javax.json.bind.annotation.JsonbCreator;
 import javax.json.bind.annotation.JsonbProperty;
@@ -21,7 +6,7 @@ import java.math.BigDecimal;
 import java.util.Optional;
 
 /**
- * @author Roman Grigoriadi
+ * @author misl
  */
 public class CreatorConstructorPojoOptional {
 
