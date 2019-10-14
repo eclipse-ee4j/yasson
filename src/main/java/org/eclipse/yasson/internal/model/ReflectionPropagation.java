53714521993143f5d65870dee7359b9ae4ef696c
Gyúróczki Gergő <gergonoorbi@gmail.com>
2019-10-14 09:58:14 +0200

    Refactor SetValueCommand
    Signed-off-by: Gyúróczki Gergő <gergonoorbi@gmail.com>

--- src/main/java/org/eclipse/yasson/internal/model/ReflectionPropagation.java
+++ src/main/java/org/eclipse/yasson/internal/model/ReflectionPropagation.java
@@ -12,10 +12,14 @@
  ******************************************************************************/
 package org.eclipse.yasson.internal.model;
 
-import java.lang.reflect.*;
-import java.util.*;
-import javax.json.bind.*;
-import javax.json.bind.config.*;
+import javax.json.bind.config.PropertyVisibilityStrategy;
+import javax.json.bind.JsonbException;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.IllegalAccessException;
+import java.util.Objects;
 
 /**
  * @author Roman Grigoriadi
@@ -42,7 +46,7 @@ public class ReflectionPropagation extends PropertyValuePropagation {
                 getValueCommand = method::invoke;
                 break;
             case SET:
-                setValueCommand = new SetWithSetter(method);
+                setValueCommand = method::invoke;
                 break;
             default: throw new IllegalStateException("Unknown mode");
         }
@@ -60,15 +64,28 @@ public class ReflectionPropagation extends PropertyValuePropagation {
                 getValueCommand = field::get;
                 break;
             case SET:
-                setValueCommand = new SetWithField(field);
+                setValueCommand = field::set;
                 break;
             default: throw new IllegalStateException("Unknown mode");
         }
     }
 
+    /**
+     * Sets a value with reflection on {@link java.lang.reflect.Field field} or {@link java.lang.reflect.Method setter}.
+     *
+     * @param object object to invoke set value on, not null.
+     * @param value object to be set, nullable.
+     * @throws JsonbException if reflection fails.
+     */
     @Override
     void setValue(Object object, Object value) {
-        setValueCommand.setValue(object, value);
+    	Objects.requireNonNull(object);
+    	
+        try {
+        	setValueCommand.setValue(object, value);
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            throw new JsonbException("Error getting value on: " + object, e);
+        }
     }
 
     /**
