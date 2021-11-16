dd42d0a3ace1a167e3d3ecbea4510d85f1bc9f34
rmartinc <rmartinc@redhat.com>
2021-11-16 09:57:33 +0100

    Issue #500: Serialization of a Map fails if the key uses a custom Serializer (#501)
    Signed-off-by: rmartinc <rmartinc@redhat.com>

--- src/main/java/org/eclipse/yasson/internal/serializer/MapToEntriesArraySerializer.java
+++ src/main/java/org/eclipse/yasson/internal/serializer/MapToEntriesArraySerializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -106,8 +106,10 @@ public class MapToEntriesArraySerializer<K, V> implements MapSerializer.Delegate
         obj.forEach((key, value) -> {
             generator.writeStartObject();
             generator.writeKey(keyEntryName);
+            serializer.serializeKey();
             serializer.serializeItem(key, generator, ctx);
             generator.writeKey(valueEntryName);
+            serializer.serializeValue();
             serializer.serializeItem(value, generator, ctx);
             generator.writeEnd();
         });
