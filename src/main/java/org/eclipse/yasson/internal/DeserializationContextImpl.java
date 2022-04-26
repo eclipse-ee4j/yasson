0f76d0132826d018e3415d7eec09e82c4d4e5674
dansiviter <dansiviter@gmail.com>
2022-04-26 11:58:22 +0100

    Remove log noise (#548)
    Remove log noise

--- src/main/java/org/eclipse/yasson/internal/DeserializationContextImpl.java
+++ src/main/java/org/eclipse/yasson/internal/DeserializationContextImpl.java
@@ -15,7 +15,6 @@ package org.eclipse.yasson.internal;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 import jakarta.json.bind.JsonbException;
 import jakarta.json.bind.serializer.DeserializationContext;
@@ -31,9 +30,6 @@ import org.eclipse.yasson.internal.properties.Messages;
  * Deserialization context implementation.
  */
 public class DeserializationContextImpl extends ProcessingContext implements DeserializationContext {
-
-    private static final Logger LOGGER = Logger.getLogger(DeserializationContextImpl.class.getName());
-
     private final List<Runnable> delayedSetters = new ArrayList<>();
     private JsonParser.Event lastValueEvent;
     private Customization customization = ClassCustomization.empty();
@@ -141,10 +137,8 @@ public class DeserializationContextImpl extends ProcessingContext implements Des
             ModelDeserializer<JsonParser> modelDeserializer = getJsonbContext().getChainModelCreator().deserializerChain(type);
             return (T) modelDeserializer.deserialize(parser, this);
         } catch (JsonbException e) {
-            LOGGER.severe(e.getMessage());
             throw e;
-        } catch (Exception e) {
-            LOGGER.severe(e.getMessage());
+        } catch (RuntimeException e) {
             throw new JsonbException(Messages.getMessage(MessageKeys.INTERNAL_ERROR, e.getMessage()), e);
         }
     }
