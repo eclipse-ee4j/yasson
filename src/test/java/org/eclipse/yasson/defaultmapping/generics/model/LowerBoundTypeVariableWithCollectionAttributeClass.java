287c9dc1f790684fec635b738d069e939cc57f7f
amoscatelli <alessandro.moscatelli@live.com>
2022-12-03 18:07:51 +0100

    copyright fix

--- src/test/java/org/eclipse/yasson/defaultmapping/generics/model/LowerBoundTypeVariableWithCollectionAttributeClass.java
+++ src/test/java/org/eclipse/yasson/defaultmapping/generics/model/LowerBoundTypeVariableWithCollectionAttributeClass.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright (c) 2015, 2022 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0,
+ * or the Eclipse Distribution License v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+ */
+
 package org.eclipse.yasson.defaultmapping.generics.model;
 
 import java.util.Collection;
 
+/**
+ * @author Alessandro Moscatelli
+ */
 public class LowerBoundTypeVariableWithCollectionAttributeClass<T extends Shape> {
     
     private Collection<AnotherGenericTestClass<Integer, T>> value;
