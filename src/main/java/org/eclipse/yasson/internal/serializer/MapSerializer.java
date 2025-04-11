75242d3019c2a6feceb5ca563a0f500cde2b7c19
Joerg Baesner <jbaesner@redhat.com>
2025-04-11 08:00:33 +0200

    Fixed #663
    rebased on current 'master'
    incorporated review comments

--- src/main/java/org/eclipse/yasson/internal/serializer/MapSerializer.java
+++ src/main/java/org/eclipse/yasson/internal/serializer/MapSerializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2025 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -14,12 +14,10 @@ package org.eclipse.yasson.internal.serializer;
 
 import java.util.Map;
 
-import jakarta.json.bind.serializer.JsonbSerializer;
 import jakarta.json.stream.JsonGenerator;
 
 import org.eclipse.yasson.internal.JsonbContext;
 import org.eclipse.yasson.internal.SerializationContextImpl;
-import org.eclipse.yasson.internal.components.ComponentBindings;
 import org.eclipse.yasson.internal.serializer.types.TypeSerializers;
 
 /**
