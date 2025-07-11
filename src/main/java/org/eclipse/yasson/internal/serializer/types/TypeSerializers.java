c5a0c105579d6f21ba5d1ac1751c8a2c213f2c58
David Kral <david.k.kral@oracle.com>
2025-07-11 13:30:26 +0200

    minor improvement
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- src/main/java/org/eclipse/yasson/internal/serializer/types/TypeSerializers.java
+++ src/main/java/org/eclipse/yasson/internal/serializer/types/TypeSerializers.java
@@ -54,6 +54,7 @@ import jakarta.json.JsonString;
 import jakarta.json.JsonValue;
 import jakarta.json.bind.JsonbException;
 import jakarta.json.bind.serializer.JsonbSerializer;
+
 import org.eclipse.yasson.internal.JsonbContext;
 import org.eclipse.yasson.internal.model.customization.Customization;
 import org.eclipse.yasson.internal.serializer.ModelSerializer;
